dc4433ecf48bd65e7cce4de800e7b1fbb9cf98c1
Richard Huynh <huynhtastic@gmail.com>
2023-02-04 15:18:29 -0600

    feat: build deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+
+async function main() {
+  const HelloWorld = await ethers.getContractFactory("HelloWorld");
+
+  const helloWorld = await HelloWorld.deploy("Hello World!");
+  console.log("Contract deployed to address:", helloWorld.address);
+}
+
+main()
+.then(() => process.exit(0))
+.catch(err => {
+  console.error(err);
+  process.exit(1);
+})
